5f770dd3dae5468e67b13c2ced795ef1ef2de8fa
Aleksey <a_zhuravlyov@bk.ru>
2022-08-04 13:26:49 +0300

    Finish the Ex_1

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b)
+
+{
+    Console.WriteLine("Они равны");
+}
+
+else if(a > b)
+
+{
+    Console.Write("Большее число - ");
+    Console.WriteLine(a);
+    Console.Write("Меньшее число - ");
+    Console.WriteLine(b);
+}
+
+else
+
+{
+    Console.Write("Большее число - ");
+    Console.WriteLine(b);
+    Console.Write("Меньшее число - ");
+    Console.WriteLine(a);
+}
