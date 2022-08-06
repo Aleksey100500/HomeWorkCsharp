678678ef27eb413040a0a575520b195277f16843
Aleksey <a_zhuravlyov@bk.ru>
2022-08-06 21:55:24 +0300

    Add Ex_4 the Start

--- Exercise_4/Program.cs
+++ Exercise_4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число N, ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+if (N > 0)
+{
+    while(count <= N)
+    {
+    int [] ch_numbers;
+    int [] nechet_numbers;
+
+    if ((count % 2) == 0)
+
+    {
+        ch_numbers[] == count;
+    }
+
+
+    Console.WriteLine("");
+    count++;
+    }
+}
